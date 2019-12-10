1d0a17feb02fe210089e854ce6fa3af408d35315
Joonas Harjukallio <harjukallio@Joonass-MacBook-Pro.local>
2019-12-10 22:50:54 +0200

    Add bootstrap for symlinking

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+link () {
+	echo "Symlink dotfiles to home directory"
+	echo "Shall we continue? (y/n)"
+	read resp
+	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+			ln -sv "$PWD/$file" "$HOME"
+		done
+		echo "Symlinking complete"
+	else
+		echo "Symlinking cancelled by user"
+		return 1
+	fi
+}
+
+link
